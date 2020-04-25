524c67b8bdbeb0ef6041871f22e49a1636fb55c5
Abhay Kushwaha <60154404+abkus1@users.noreply.github.com>
2020-04-25 15:07:44 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
-public class HelloWorld{
+mepublic class HelloWorld{
 	public static void main(String[] args){
-	System.out.println("....**** Hello World & welcome to Jenkins training ****....");
+	System.out.println("....**** Made some changes ****....");
 	}
 }
